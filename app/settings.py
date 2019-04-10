fb333a78c21ce058656544f0788f34919aa4dd76
Erick Rivas <erick_rivas@outlook.com>
2019-04-09 21:14:07 -0500

    Add static files support

--- app/settings.py
+++ app/settings.py
@@ -3,7 +3,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('DJANGO_SECRET')
+SECRET_KEY = 'fup+swltefA9efredrufihUSTO!wam?c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -110,4 +110,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
