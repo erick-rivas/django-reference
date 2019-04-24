8c5b2cf5f31261269220e064faa1503e3dd80be6
Erick Rivas <erick_rivas@outlook.com>
2019-04-23 20:45:10 -0500

    Update https configuration

--- serializers/helpers/util.py
+++ serializers/helpers/util.py
@@ -8,3 +8,12 @@ class Util:
             if not field.name in exclude:
                 attrs.append(str(field.name))
         return tuple(attrs)
+
+    @staticmethod
+    def get_inner_attr_list(model, *include):
+        attrs = []
+        fields = model._meta.get_fields()
+        for field in fields:
+            if not field.is_relation or field.name in include:
+                attrs.append(str(field.name))
+        return tuple(attrs)
