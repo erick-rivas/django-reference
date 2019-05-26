d371252f652edc8d3ad649f3bff6b32749ecb6f7
Erick Rivas <erick_rivas@outlook.com>
2019-05-25 21:36:38 -0500

    Add env to prod and dev

--- app/wsgi.py
+++ app/wsgi.py
@@ -3,7 +3,9 @@ import dotenv
 
 from django.core.wsgi import get_wsgi_application
 
-dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+env_file = '.env.dev'
+if 'IS_PROD' in os.environ: env_file = '.env.prod'
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), env_file))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
 application = get_wsgi_application()
--- routes/helpers/viewsets.py
+++ routes/helpers/viewsets.py
@@ -10,14 +10,23 @@ from rest_framework.permissions import IsAuthenticated
 
 
 class BaseViewSet(viewsets.GenericViewSet):
+
     """ Enable security
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     """
+
     def destroy(self, request, pk=None):
         model = get_object_or_404(self.queryset, pk=pk)
         model.delete()
         return Response({'id': int(pk)}, status=status.HTTP_200_OK)
+
+    def update(self, request, pk=None):
+        obj = self.get_object()
+        serializer = self.get_serializer(obj, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
 class ReadOnlyViewSet(
     BaseViewSet,
