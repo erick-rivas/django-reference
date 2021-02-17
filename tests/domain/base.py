9b2c927549182afcb562822146bf2b2b16da25e0
Erick Rivas <erick_rivas@outlook.com>
2021-02-17 00:20:45 -0600

    :raised_hands: Start test module

---
+++ tests/domain/base.py
@@ -0,0 +1,11 @@
+import json
+from unittest import TestCase
+
+
+class TestBase(TestCase):
+
+    def setUp(self):
+        pass
+
+    def assetJson(self, json_a, json_b):
+        self.assertEqual(json.dumps(json_a, indent=2, sort_keys=True), json.dumps(json_b, indent=2, sort_keys=True))
