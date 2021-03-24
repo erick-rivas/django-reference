72cf067841457531a8e507c61eaa7d5c2f66fe0c
Erick Rivas <erick_rivas@outlook.com>
2021-03-24 01:22:55 -0600

    Update docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,9 @@
 #
 import os
 import sys
+
 import django
+
 sys.path.insert(0, os.path.abspath('..'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'app.settings'
 django.setup()
@@ -22,13 +24,17 @@ django.setup()
 project = 'django-reference'
 author = 'erick-rivas'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'myst_parser', "sphinx_rtd_theme"]
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'myst_parser',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = []
@@ -38,15 +44,16 @@ templates_path = []
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = [".venv"]
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_static_path = []
+import sphinx_redactor_theme
+html_theme = "sphinx_redactor_theme"
+html_theme_path = [sphinx_redactor_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
