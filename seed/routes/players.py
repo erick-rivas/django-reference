179ba53b9b1a9853f2885b7b2ce4f408eeee0b22
Erick Rivas <erick_rivas@outlook.com>
2021-04-04 10:03:31 -0500

    Remove duplicated code of seed routes

--- seed/routes/players.py
+++ seed/routes/players.py
@@ -4,10 +4,7 @@ __Seed builder__v0.2.0
   Modify via builder
 """
 
-from django.shortcuts import get_object_or_404
 from rest_framework import mixins
-from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 from seed.routes.viewset import ViewSet
 
@@ -24,17 +21,6 @@ class PlayerViewSet(
     GenericViewSet):
     serializer_class = PlayerSerializer
     queryset = Player.objects.all()
-    def destroy(self, request, pk=None):
-        model = get_object_or_404(self.queryset, pk=pk)
-        model.delete()
-        return Response({'id': int(pk)}, status=status.HTTP_200_OK)
-    def update(self, request, pk=None):
-        obj = self.get_object()
-        serializer = self.get_serializer(obj, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
     def get_queryset(self):
         user = self.request.user
         return Player.filter_permissions(
--- seed/routes/viewset.py
+++ seed/routes/viewset.py
@@ -3,12 +3,27 @@ __Seed builder__v0.2.0
   (Read_only) Builder helper
 """
 
+from app.settings import get_env
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from app.settings import get_env
+from rest_framework.response import Response
 
 
 class ViewSet:
     if get_env('ENABLE_AUTH'):
         authentication_classes = (TokenAuthentication,)
-        permission_classes = (IsAuthenticated, )
+        permission_classes = (IsAuthenticated,)
+
+    def destroy(self, request, pk=None):
+        model = get_object_or_404(self.queryset, pk=pk)
+        model.delete()
+        return Response({'id': int(pk)}, status=status.HTTP_200_OK)
+
+    def update(self, request, pk=None):
+        obj = self.get_object()
+        serializer = self.get_serializer(obj, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
