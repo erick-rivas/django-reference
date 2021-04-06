56819aa3560693b62f360ae1b6e66287cd4c634a
Erick Rivas <erick_rivas@outlook.com>
2021-04-05 21:11:04 -0500

    Fix setup.bat env bug

--- bin/setup.bat
+++ bin/setup.bat
@@ -1,13 +1,12 @@
 # Configure docker .env
 @echo off
-del ./.env
-echo "# DOCKER PORTS (DON'T MODIFY)" > ".env"
-echo "DJANGO_PORT=8008" >> ".env"
-echo "POSTGRES_PORT=5435" >> ".env"
-echo "REDIS_PORT=6377" >> ".env"
-
-# Configure docker .env
-cp ./bin/docker/.docker.env ./.env
+del bin/docker/.env
+echo "# DOCKER PORTS (DON'T MODIFY)" > "bin/docker/.env"
+echo "### DON'T MODIFY ###" >> "bin/docker/.env"
+echo "" >> "bin/docker/.env"
+echo "DJANGO_PORT=8008" >> "bin/docker/.env"
+echo "POSTGRES_PORT=5435" >> "bin/docker/.env"
+echo "REDIS_PORT=6377" >> "bin/docker/.env"
 
 # Delete previous containers
 docker-compose -f bin/docker/docker-compose.dev.yml down
@@ -16,7 +15,7 @@ docker-compose -f bin/docker/docker-compose.dev.yml down
 docker-compose -f bin/docker/docker-compose.dev.yml build
 
 # Set execute permissions to bin
-docker-compose -f bin/docker/docker-compose.dev.yml run django chmod +x bin/*;chmod +x bin/docker/* 
+docker-compose -f bin/docker/docker-compose.dev.yml run django chmod +x bin/*;chmod +x bin/docker/*
 
 # Creating .env.devs
 docker-compose -f bin/docker/docker-compose.dev.yml run django bin/docker/env-dev.sh 8008 5435 6377
