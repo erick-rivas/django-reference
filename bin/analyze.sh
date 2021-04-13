99888861e8cb863fbec10054cded1181acdca83a
Erick Rivas <erick_rivas@outlook.com>
2021-04-13 02:29:04 -0500

    Remove codacy from docker-compose

--- bin/analyze.sh
+++ bin/analyze.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "== Analyzing with pylint"
-docker-compose -f bin/docker/docker-compose.dev.yml run codacy-cli analyze --tool pylint --force-file-permissions
+codacy-analysis-cli analyze --tool pylint --force-file-permissions
 echo "== Analyzing with bandit"
-docker-compose -f bin/docker/docker-compose.dev.yml run codacy-cli analyze --tool bandit --force-file-permissions
+codacy-analysis-cli analyze --tool bandit --force-file-permissions
